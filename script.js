894a1e14cf21fdbe9663dd5ebd47db5e7fe0bad5
MusayevDoniyor <musayevd1015@gmail.com>
2024-11-23 15:03:17 +0500

    done fully

--- script.js
+++ script.js
@@ -7,4 +7,35 @@ document.addEventListener("DOMContentLoaded", () => {
     nav_list.classList.toggle("show");
     nav_list.classList.toggle("hide");
   });
+
+  const scrollToTopBtn = document.getElementById("scrollToTop");
+  const scrollToTopText = document.getElementById("scrollToTopText");
+  const scrollToTopWrapper = document.getElementById("scrollToTopWrapper");
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 200) {
+      scrollToTopBtn.style.display = "block";
+    } else {
+      scrollToTopBtn.style.display = "none";
+    }
+  });
+
+  scrollToTopBtn.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
+
+  scrollToTopWrapper.addEventListener("mouseover", () => {
+    scrollToTopText.style.display = "block";
+    scrollToTopBtn.style.backgroundColor = "#d3d3d3";
+    scrollToTopBtn.style.color = "#000";
+  });
+
+  scrollToTopWrapper.addEventListener("mouseout", () => {
+    scrollToTopText.style.display = "none";
+    scrollToTopBtn.style.backgroundColor = "#000";
+    scrollToTopBtn.style.color = "#fff";
+  });
 });
